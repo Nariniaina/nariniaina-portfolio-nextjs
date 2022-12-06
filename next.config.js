633173e3c6f5418d8efa320c2f07c8262bd89c15
Nariniaina <ramanantsalamanary@gmail.com>
2022-12-07 02:03:00 +0300

    search engine vercel

--- next.config.js
+++ next.config.js
@@ -5,6 +5,21 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+      headers.push({
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+        source: "/:path*",
+      });
+    }
+    return headers;
+  },
 };
 
 module.exports = nextConfig;
