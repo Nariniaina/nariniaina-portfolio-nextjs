b99d41e09ffe374c43ecd6d46d181e314dbd321a
Nariniaina <ramanantsalamanary@gmail.com>
2022-12-07 02:27:14 +0300

    search engine ready

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://nariniaina-portfolio.vercel.app",
+  generateRobotsTxt: true, // (optional)
+};
