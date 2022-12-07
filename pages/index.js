976b3b9df011289efb29d29bd3a322c67291ad97
Nariniaina <ramanantsalamanary@gmail.com>
2022-12-07 03:35:52 +0300

    add pin from Madagascar

--- pages/index.js
+++ pages/index.js
@@ -31,88 +31,107 @@ export default function Home() {
         <meta name="keywords" content="nariniaina" />
       </Head>
       <div>
-        <nav className="max-w-7xl mx-auto flex flex-row items-center justify-between gap-5 py-5 px-6">
-          <div
-            className="block sm:hidden hover:cursor-pointer rounded-full group p-2 
-          hover:bg-[#ff8900] transition-all ease-in-out duration-500"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              className="min-w-[25px] min-h-[25px] max-w-[25px] max-h-[25px] 
+        <header>
+          <nav className="max-w-7xl mx-auto flex flex-row items-center justify-between gap-5 py-5 px-6">
+            <div
+              className="block sm:hidden hover:cursor-pointer rounded-full group p-2 
+            hover:bg-[#ff8900] transition-all ease-in-out duration-500"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                className="min-w-[25px] min-h-[25px] max-w-[25px] max-h-[25px] 
               group-hover:text-white text-slate-600 dark:text-gray-400
               transition-all ease-in-out duration-500"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-              />
-            </svg>
-          </div>
-          <ul className="tracking-wide hidden sm:flex flex-row gap-10 text-slate-800 text-lg">
-            <li className="cursor-pointer border-b-4 border-transparent hover:border-orange-400 transition-all ease-in-out duration-500">
-              A propos de moi
-            </li>
-            <li className="cursor-pointer border-b-4 border-transparent hover:border-orange-400 transition-all ease-in-out duration-500">
-              Projets
-            </li>
-            <li className="cursor-pointer border-b-4 border-transparent hover:border-orange-400 transition-all ease-in-out duration-500">
-              Compétences
-            </li>
-          </ul>
-          <button className="tracking-wide bg-gray-100 transition-all hover:bg-orange-600 hover:scale-105 hover:text-white ease-in-out duration-500 font-semibold rounded-full px-4 py-2">
-            Contactez-moi
-          </button>
-        </nav>
-        <div className="grid grid-cols-2 lg:flex-row max-w-7xl mx-auto w-full">
-          <div className="col-span-2 lg:col-span-1 order-1 lg:order-none mt-5 lg:mt-[55px] px-6">
-            <div className="flex flex-col justify-center lg:justify-start lg:max-w-xl">
-              <h1 className="leading-[55px] sm:leading-[65px] title-font transition-all ease-in-out duration-500 text-4xl sm:text-6xl text-slate-900 break-words">
-                Mon nom est R.Nariniaina
-              </h1>
-              <p className="mt-7 text-xl leading-[30px] text-slate-700">
-                Je suis Nariniaina. Avec 4 ans d'expérience. Je perfectionne mon
-                art qu'est le développement en tant qu'artisan de la
-                technologie. Pour réaliser mon rêve et le votre.{" "}
-              </p>
-              <div className="mt-9 flex flex-wrap max-w-sm gap-4 text-xl">
-                <span className="transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer whitespace-nowrap rounded-lg text-black font-semibold bg-[#fcb03d] w-min px-3 py-1">
-                  Javascript
-                </span>
-                <span className="transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer whitespace-nowrap rounded-lg text-white font-semibold bg-[#e66910] w-min px-3 py-1">
-                  NextJS, ReactJS
-                </span>
-                <span className="transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer whitespace-nowrap rounded-lg text-white font-semibold bg-[#241f21] w-min px-3 py-1">
-                  NodeJS, Express
-                </span>
-                <span className="transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer whitespace-nowrap rounded-lg text-black font-semibold bg-[#e1e1e1] w-min px-3 py-1">
-                  MongoDB/Firebase
-                </span>
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                />
+              </svg>
+            </div>
+            <ul className="tracking-wide hidden sm:flex flex-row gap-10 text-slate-800 text-lg">
+              <li className="cursor-pointer border-b-4 border-transparent hover:border-orange-400 transition-all ease-in-out duration-500">
+                A propos de moi
+              </li>
+              <li className="cursor-pointer border-b-4 border-transparent hover:border-orange-400 transition-all ease-in-out duration-500">
+                Projets
+              </li>
+              <li className="cursor-pointer border-b-4 border-transparent hover:border-orange-400 transition-all ease-in-out duration-500">
+                Compétences
+              </li>
+            </ul>
+            <div className="flex flex-row items-center gap-5">
+              <button className="tracking-wide bg-gray-100 transition-all hover:bg-orange-600 hover:scale-105 hover:text-white ease-in-out duration-500 font-semibold rounded-full px-4 py-2">
+                Contactez-moi
+              </button>
+              <div className="hidden md:flex flex-row items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="text-green-500 min-h-[30px] min-w-[30px] max-h-[30px] max-w-[30px]"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                </svg>
+                <h4 className="text-slate-800 font-semibold text-lg">
+                  From Madagascar
+                </h4>
               </div>
             </div>
-          </div>
-          <div className="group col-span-2 lg:col-span-1 flex items-center justify-center h-full relative">
-            <div
-              className="hidden lg:flex w-min rounded-3xl group-hover:scale-105
+          </nav>
+          <div className="grid grid-cols-2 lg:flex-row max-w-7xl mx-auto w-full">
+            <div className="col-span-2 lg:col-span-1 order-1 lg:order-none mt-5 lg:mt-[55px] px-6">
+              <div className="flex flex-col justify-center lg:justify-start lg:max-w-xl">
+                <h1 className="leading-[55px] sm:leading-[75px] title-font transition-all ease-in-out duration-500 text-4xl sm:text-6xl text-slate-900 break-words">
+                  Mon nom est R.Nariniaina
+                </h1>
+                <p className="mt-7 text-xl leading-[30px] text-slate-700">
+                  Je suis Nariniaina. Avec 4 ans d'expérience. Je perfectionne
+                  mon art qu'est le développement en tant qu'artisan de la
+                  technologie. Pour réaliser mon rêve et le votre.{" "}
+                </p>
+                <div className="mt-9 flex flex-wrap max-w-sm gap-4 text-xl">
+                  <span className="transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer whitespace-nowrap rounded-lg text-black font-semibold bg-[#ffd495] w-min px-3 py-1">
+                    Javascript
+                  </span>
+                  <span className="transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer whitespace-nowrap rounded-lg text-white font-semibold bg-[#e66910] w-min px-3 py-1">
+                    NextJS, ReactJS
+                  </span>
+                  <span className="transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer whitespace-nowrap rounded-lg text-white font-semibold bg-[#241f21] w-min px-3 py-1">
+                    NodeJS, Express
+                  </span>
+                  <span className="transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer whitespace-nowrap rounded-lg text-black font-semibold bg-[#e1e1e1] w-min px-3 py-1">
+                    MongoDB/Firebase
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="group col-span-2 lg:col-span-1 flex items-center justify-center h-full relative">
+              <div
+                className="hidden lg:flex w-min rounded-3xl group-hover:scale-105
                 absolute bottom-[25%] left-[45px] flex-col gap-1 bg-[#241f21]
                 px-6 py-5 transition-all ease-in-out duration-500 group-hover:opacity-95"
-            >
-              <h4 className="text-5xl font-medium text-white">04+</h4>
-              <p className="text-lg whitespace-nowrap text-white">
-                ans d'expériences
-              </p>
-            </div>
-            <img
-              className="group-hover:scale-90 z-[-1] rounded-full object-contain
+              >
+                <h4 className="text-5xl font-medium text-white">04+</h4>
+                <p className="text-lg whitespace-nowrap text-white">
+                  ans d'expériences
+                </p>
+              </div>
+              <img
+                className="group-hover:scale-90 z-[-1] rounded-full object-contain
               transition-all ease-in-out duration-500"
-              width="450"
-              height="450"
-              src="/assets/profile-pic.png"
-              alt="nariniaina-picture"
-            />
+                width="450"
+                height="450"
+                src="/assets/profile-pic.png"
+                alt="nariniaina-picture"
+              />
+            </div>
           </div>
-        </div>
+        </header>
         <div className="max-w-7xl mx-auto px-6 mt-10">
           <div className="flex flex-row justify-between gap-5">
             <h1 className="text-black text-4xl tracking-widerr">
